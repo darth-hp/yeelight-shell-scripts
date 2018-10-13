f9c64cfa09be3d0718efac343c6c4ecc4efbc9a6
Elias <marks.eliasariel@gmail.com>
2018-10-13 21:16:56 +0200

    Added toggle mode

---
+++ yeelight-toggle.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+IDMSG="ID must be a number"
+
+usage(){
+	echo "Usage: $( basename $0 ) <ID>"
+	echo "  $IDMSG"
+}
+
+[[ "$#" -ne 1 ]] && usage && exit 1
+[[ ! "$1" =~ ^[0-9]+$ ]] && echo "ERROR: $IDMSG" && exit 1
+
+ct='"method":"toggle","params":[,,]'
+
+$( dirname $0 )/yeelight.sh "$1" "$ct"
